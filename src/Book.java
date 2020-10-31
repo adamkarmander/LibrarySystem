c65730d75a4de3ca8e3c81005b41a9b8ca2208c8
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2020-10-31 21:13:35 +0100

    lagt in products metoder

--- src/Book.java
+++ src/Book.java
@@ -16,9 +16,21 @@ public class Book extends Product {
 		return author;
 	}
 
+	@Override
+	public String productCsvRec() {
+		super.productCsvRec();
+		return productCsvRec();
+	}// Såhär vi får tag på metoden productCsvRec?
+
+	public String productToString() {
+		super.productToString();
+		return productToString();
+	}// Såhär vi får tag på metoden ProducToString?
+
 	public String bookCsvRec() {
 		return String.format("%d, %s", pages, author);
-	}
+
+	}// Hur skriva regex för int?
 
 	public String bookToString() {
 		return String.format("%d  %s.");
