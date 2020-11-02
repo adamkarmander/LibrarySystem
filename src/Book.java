903845233fc9779927b47f641cfeca2193de18b1
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2020-11-02 15:49:54 +0100

    importerat CSV fil. Sett över andra konstruktorn i Book, den vill ej fungera

--- src/Book.java
+++ src/Book.java
@@ -7,8 +7,9 @@ public class Book extends Product {
 		this.pages = pages;
 		this.author = author;
 	}
-	
-	public Book(int articleNumber, String productType, String productName, int productValue, int pages, String author, Customer borrowingCustomer) {
+
+	public Book(int articleNumber, String productType, String productName, int productValue, int pages, String author,
+			Costumer borrowingCustomer) {
 		super(articleNumber, productType, productName, productValue, borrowingCustomer);
 		this.pages = pages;
 		this.author = author;
@@ -17,7 +18,7 @@ public class Book extends Product {
 	public int getPages() {
 		return pages;
 	}
-	
+
 	public void setPages(int newPages) {
 		pages = newPages;
 	}
@@ -25,29 +26,21 @@ public class Book extends Product {
 	public String getAuthor() {
 		return author;
 	}
-	
+
 	public void setAuthor(String newAuthor) {
 		author = newAuthor;
 	}
 	/*
-	@Override
-	public String productCsvRec() {
-		super.productCsvRec();
-		return productCsvRec();
-	}// Såhär vi får tag på metoden productCsvRec?
-
-	public String productToString() {
-		super.productToString();
-		return productToString();
-	}// Såhär vi får tag på metoden ProducToString?
-
-	public String bookCsvRec() {
-		return String.format("%d, %s", pages, author);
-
-	}// Hur skriva regex för int?
-
-	public String bookToString() {
-		return String.format("%d  %s.");
-	}
-	*/
+	 * @Override public String productCsvRec() { super.productCsvRec(); return
+	 * productCsvRec(); }// Såhär vi får tag på metoden productCsvRec?
+	 * 
+	 * public String productToString() { super.productToString(); return
+	 * productToString(); }// Såhär vi får tag på metoden ProducToString?
+	 * 
+	 * public String bookCsvRec() { return String.format("%dm, %s", pages, author);
+	 * 
+	 * }// Hur skriva regex för int?
+	 * 
+	 * public String bookToString() { return String.format("%dm  %s."); }
+	 */
 }
