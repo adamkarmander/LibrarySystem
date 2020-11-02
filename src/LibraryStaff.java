d6127b4c270cdc5290b0c3dc59b94934611b1aa6
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2020-11-02 14:09:38 +0100

    Försök till CSV

--- src/LibraryStaff.java
+++ src/LibraryStaff.java
@@ -1,118 +1,122 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.csv.*;
 import java.util.Scanner;
 
 public class LibraryStaff {
 	enum Command {
-		LIST,
-        CHECKOUT,
-        CHECKIN,
-        REGISTER,
-        DEREGISTER,
-        INFO,
-        QUIT,
-        UNKNOWN
+		LIST, CHECKOUT, CHECKIN, REGISTER, DEREGISTER, INFO, QUIT, UNKNOWN
 	}
 
 	public static Command parseCommand(String userInput) {
-        String commandString = userInput.split(" ")[0];
-        switch (commandString) {
-	        case "list":
-	            return Command.LIST;
-	        case "checkout":
-	            return Command.CHECKOUT;
-	        case "checkin":
-	            return Command.CHECKIN;
-	        case "register":
-	            return Command.REGISTER;
-	        case "deregister":
-	            return Command.DEREGISTER;
-	        case "info":
-	            return Command.INFO;
-	        case "quit":
-	            return Command.QUIT;
-	        default:
-	        	return Command.UNKNOWN;
-        }
-    }
+		String commandString = userInput.split(" ")[0];
+		switch (commandString) {
+		case "list":
+			return Command.LIST;
+		case "checkout":
+			return Command.CHECKOUT;
+		case "checkin":
+			return Command.CHECKIN;
+		case "register":
+			return Command.REGISTER;
+		case "deregister":
+			return Command.DEREGISTER;
+		case "info":
+			return Command.INFO;
+		case "quit":
+			return Command.QUIT;
+		default:
+			return Command.UNKNOWN;
+		}
+	}
 
 	public static String parseArgument(String userInput) {
-    	if(userInput.contains(" ")) {
-            String argument = userInput.split(" ")[1];
-            return argument;
-    	}
-    	return "";
-    }
+		if (userInput.contains(" ")) {
+			String argument = userInput.split(" ")[1];
+			return argument;
+		}
+		return "";
+	}
 
 	public static void main(String[] args) {
-    	LibraryInventory lib = new LibraryInventory();
-    	
-    	Customer customer1 = new Customer("Tomas Larsson", "073-683-3307");
-    	Book book1 = new Book(12345, "Book", "Utvandrarna", 99, 573, "Vilhelm Moberg", customer1);
-    	Book book2 = new Book(13370, "Book", "Historien om Titanic", 99, 45, "Francesca Baines");
-    	Movie movie1 = new Movie(20202, "Movie", "Djungelboken", 169, 78, 7.6);
-    	lib.addProduct(book1);
-    	lib.addProduct(book2);
-    	lib.addProduct(movie1);
-    	
-    	Scanner scanner = new Scanner(System.in);
-    	while (true) {
-		    String userInput = scanner.nextLine();
-		    Command command = parseCommand(userInput);
-		    String argument = parseArgument(userInput);
-		    
-		    if (command == Command.LIST) {
-		    	System.out.println(lib.toString());
-		    } else if (command == Command.CHECKOUT) {
-		    	lib.borrowProduct(argument);
-		    } else if (command == Command.CHECKIN) {
-		    	lib.returnProduct(argument);
-		    } else if (command == Command.REGISTER) {
-		    	String type, title;
-		    	int id, value;
-		    	System.out.println("What are you registering? Book (b), Movie (m)");
-		    	type = scanner.next().toLowerCase();
-		    	System.out.println("Enter product ID:");
-		    	id = scanner.nextInt();
-		    	System.out.println("Enter title:");
-		    	title = scanner.next();
-		    	System.out.println("Enter value:");
-		    	value = scanner.nextInt();
-		    	
-		    	if(!lib.isRegistered(id)) {
-		    		if(type.equals("b")) {
-		    			int pages;
-			    		String publisher;
-			    		System.out.println("Enter number of pages:");
-			    		pages = scanner.nextInt();
-			    		System.out.println("Enter publisher:");
-			    		publisher = scanner.next();
-		    			Book book = new Book(id, "Book", title, value, pages, publisher);
-			    		lib.addProduct(book);
-		    		} else if(type.equals("m")) {
-		    			int length;
-			    		double rating;
-			    		System.out.println("Enter length:");
-			    		length = scanner.nextInt();
-			    		System.out.println("Enter rating:");
-			    		rating = scanner.nextDouble();
-			    		Movie movie = new Movie(id, "Movie", title, value, length, rating);
-			    		lib.addProduct(movie);
-		    		}
-		    		System.out.println("Successfully registered " + title + "!");
-		    	} else {
-		    		System.out.println("Error: Product with ID " + id + " is already registered.");
-		    	}
-		    } else if (command == Command.DEREGISTER) {
-		    	System.out.println(lib.deregister(argument));
-		    } else if (command == Command.INFO) {
-		    	System.out.println(lib.getInfo(argument));	
-		    } else if (command == Command.QUIT) {
-		        System.out.println("Goodbye!");
-		        scanner.close();
-		        System.exit(0);
-		    } else if(command == Command.UNKNOWN) {
-		    	System.out.println("Unknown command. Try again.");
-		    	continue;
-		    }
-    	}
-    }
-}
+		LibraryInventory lib = new LibraryInventory();
+
+		try {
+			FileWriter write = new FileWriter("library_csv.csv");
+			CSVPrinter print = new CSVPrinter(writer, CSVFormat.DEAFULT);
+			print.printRecord(2345, "Book", "the Emigrants", 99, 573, "Vilhelm Moberg");
+			print.printRecord(2346, "Bok", "Story of the Titanic", 99, 45, "Francesca Baines");
+			print.printRecord(2346, "Movie", "The Jungle Book", 169, 78, 7.6);
+		} catch (IOException e) {
+		}
+	}
+
+	Customer customer1 = new Customer("Tomas Larsson", "073-683-3307");
+	Book book1 = new Book(12345, "Book", "Utvandrarna", 99, 573, "Vilhelm Moberg", customer1);
+	Book book2 = new Book(13370, "Book", "Historien om Titanic", 99, 45, "Francesca Baines");
+	Movie movie1 = new Movie(20202, "Movie", "Djungelboken", 169, 78,
+			7.6);lib.addProduct(book1);lib.addProduct(book2);lib.addProduct(movie1);
+
+	Scanner scanner = new Scanner(System.in);while(true)
+	{
+		String userInput = scanner.nextLine();
+		Command command = parseCommand(userInput);
+		String argument = parseArgument(userInput);
+
+		if (command == Command.LIST) {
+			System.out.println(lib.toString());
+		} else if (command == Command.CHECKOUT) {
+			lib.borrowProduct(argument);
+		} else if (command == Command.CHECKIN) {
+			lib.returnProduct(argument);
+		} else if (command == Command.REGISTER) {
+			String type, title;
+			int id, value;
+			System.out.println("What are you registering? Book (b), Movie (m)");
+			type = scanner.next().toLowerCase();
+			System.out.println("Enter product ID:");
+			id = scanner.nextInt();
+			System.out.println("Enter title:");
+			title = scanner.next();
+			System.out.println("Enter value:");
+			value = scanner.nextInt();
+
+			if (!lib.isRegistered(id)) {
+				if (type.equals("b")) {
+					int pages;
+					String publisher;
+					System.out.println("Enter number of pages:");
+					pages = scanner.nextInt();
+					System.out.println("Enter publisher:");
+					publisher = scanner.next();
+					Book book = new Book(id, "Book", title, value, pages, publisher);
+					lib.addProduct(book);
+				} else if (type.equals("m")) {
+					int length;
+					double rating;
+					System.out.println("Enter length:");
+					length = scanner.nextInt();
+					System.out.println("Enter rating:");
+					rating = scanner.nextDouble();
+					Movie movie = new Movie(id, "Movie", title, value, length, rating);
+					lib.addProduct(movie);
+				}
+				System.out.println("Successfully registered " + title + "!");
+			} else {
+				System.out.println("Error: Product with ID " + id + " is already registered.");
+			}
+		} else if (command == Command.DEREGISTER) {
+			System.out.println(lib.deregister(argument));
+		} else if (command == Command.INFO) {
+			System.out.println(lib.getInfo(argument));
+		} else if (command == Command.QUIT) {
+			System.out.println("Goodbye!");
+			scanner.close();
+			System.exit(0);
+		} else if (command == Command.UNKNOWN) {
+			System.out.println("Unknown command. Try again.");
+			continue;
+		}
+	}
+}}
--- src/Product.java
+++ src/Product.java
@@ -11,8 +11,9 @@ public class Product {
 		this.productName = productName;
 		this.productValue = productValue;
 	}
-	
-	public Product(int articleNumber, String productType, String productName, int productValue, Customer borrowingCustomer) {
+
+	public Product(int articleNumber, String productType, String productName, int productValue,
+			Customer borrowingCustomer) {
 		this.articleNumber = articleNumber;
 		this.productType = productType;
 		this.productName = productName;
@@ -23,7 +24,7 @@ public class Product {
 	public int getArticleNumber() {
 		return articleNumber;
 	}
-	
+
 	public void setArticleNumber(int newArticleNumber) {
 		articleNumber = newArticleNumber;
 	}
@@ -31,7 +32,7 @@ public class Product {
 	public String getProductType() {
 		return productType;
 	}
-	
+
 	public void setProductType(String newProductType) {
 		productType = newProductType;
 	}
@@ -39,7 +40,7 @@ public class Product {
 	public String getProductName() {
 		return productName;
 	}
-	
+
 	public void setProductName(String newProductName) {
 		productName = newProductName;
 	}
@@ -47,33 +48,31 @@ public class Product {
 	public int getProductValue() {
 		return productValue;
 	}
-	
+
 	public void setProductValue(int newProductValue) {
 		productValue = newProductValue;
 	}
-	
+
 	@Override
 	public String toString() {
-		if(borrowingCustomer == null) {
+		if (borrowingCustomer == null) {
 			return articleNumber + " (" + productType + "): " + productName + ". (in stock)\n";
 		}
-		return articleNumber + " (" + productType + "): " + productName + ".\n    Borrowed by: " + borrowingCustomer.getName() + ", " + borrowingCustomer.getNumber() + "\n";
+		return articleNumber + " (" + productType + "): " + productName + ".\n    Borrowed by: "
+				+ borrowingCustomer.getName() + ", " + borrowingCustomer.getNumber() + "\n";
 	}
 	/*
-	public String productCsvRec() {
-		return String.format("%d, %s,%s", articleNumber, productType, productName);
-	} // Vilken regex för int?
-
-	public static String getProductHeader() {// Vill ha header?
-		return "articlenumber, type, name";
-	}
-
-	public static void parseProducts(String csvRec) { // Behövs metoden?
-		csvRec.split(", ");
-	}
-
-	public String productToString() {
-		return String.format("%d [%s]: %s.");
-
-	}*/
+	 * public String productCsvRec() { return String.format("%d, %s,%s",
+	 * articleNumber, productType, productName); } // Vilken regex för int?
+	 * 
+	 * public static String getProductHeader() {// Vill ha header? return
+	 * "articlenumber, type, name"; }
+	 * 
+	 * public static void parseProducts(String csvRec) { // Behövs metoden?
+	 * csvRec.split(", "); }
+	 * 
+	 * public String productToString() { return String.format("%d [%s]: %s.");
+	 * 
+	 * }
+	 */
 }
